845a9e5d21ad296d2030062767969a71f15f74eb
Saif180 <metrobazaar14@gmail.com>
2024-10-25 01:20:00 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "saifking0@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState = "Uttar Pradesh"
+
+console.log(accountId)
+
+accountEmail = "virat.com"
+accountPassword = "84638"
+accountCity = "Varanasi"
+accountState = "Goa"
+
+console.table([accountId, accountPassword, accountCity, accountEmail, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+// number => 2 to power 53
+// bigint
+// strin => ""
+// bollean => true/false
+//null=>  standalone value
+//undefined =>
+//symbol=> unique
+
+console.log(type of undefined)
